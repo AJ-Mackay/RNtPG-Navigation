e108e490c43a61a5aa60ec77fdabcf459676dbbd
Paul Mackay <paul.m.mackay85@gmail.com>
2021-02-11 14:54:16 +0000

    Dispatching Filter Actions

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
+import DefaultText from '../components/DefaultText';
 
 import MealList from '../components/MealList';
 import { CATEGORIES } from '../data/dummy-data';
@@ -14,6 +16,12 @@ const CategoryMealScreen = props => {
         meal => meal.categoryIds.indexOf(catId) >= 0
     );
 
+    if (displayedMeals.length === 0) {
+        return <View style={styles.content}>
+            <DefaultText>No meals found, maybe check your filters?</DefaultText>
+        </View>
+    }
+
     return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
 
@@ -26,5 +34,13 @@ CategoryMealScreen.navigationOptions = navigationData => {
         headerTitle: selectedCategory.title
     };
 };
+
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default CategoryMealScreen;
